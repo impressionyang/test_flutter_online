f87b868ae7d6b6b323eaa6752509ac905cff8a4c
Alvin_Young <impressionyang@gmail.com>
2023-11-15 09:41:51 +0000

    add listviw to contain the random word list

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,54 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       debugShowCheckedModeBanner: true,
       title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new MyRandomWords(),
-        ),
-      ),
+      home: new MyRandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<MyRandomWords> {
+  final List<WordPair> _wordpairsList = <WordPair>[];
+  final TextStyle _biggerfont = const TextStyle(fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
     final WordPair randomwords = new WordPair.random();
-    return new Text(randomwords.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('data'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          final int index = i ~/ 2;
+          if (index >= _wordpairsList.length) {
+            _wordpairsList.addAll(generateWordPairs().take(1));
+          }
+
+          if (i.isOdd) {
+            // print('now i $i, index is $index, add devider');
+            return new Divider(
+              thickness: 3.0,
+            );
+          } else {
+            // print('now i $i, index is $index, add row');
+            return _buildRow(_wordpairsList[index]);
+          }
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerfont,
+      ),
+    );
   }
 }
 
