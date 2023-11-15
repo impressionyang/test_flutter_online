ac7e76187a0e9a3ac7907afcc15047dd55ddd4f3
Alvin_Young <impressionyang@gmail.com>
2023-11-15 03:46:56 +0000

    add radom english words

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var word_pairs = new WordPair.random();
     return new MaterialApp(
+      debugShowCheckedModeBanner: true,
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: new Center(
+          child: new Text(word_pairs.asPascalCase),
         ),
       ),
     );
